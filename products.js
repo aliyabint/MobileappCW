89edfbb4280c1aa8efcd3d06da15bbcf8bb724d8
Aliya Bint <aliyabint.aa@gmail.com>
2023-11-14 11:16:34 +0400

    htmll

---
+++ products.js
@@ -0,0 +1,95 @@
+let products =[
+    {
+        id:1001,
+        title:"Dance Classes",
+        description: "Unlock the rhythm within and join our dance classes where every step tells a story. From beginners to seasoned dancers, our classes are a vibrant blend of technique and expression, fostering a community where movement becomes a celebration of joy and self-discovery.",
+        price: 200,
+        image:"images/dance.png",
+        availableInventory: 10,
+        rating: 4
+    
+    },
+    {
+        id:1002,
+        title:"Art Classes",
+        description: "Discover your artistic voice in our dynamic art classes, where imagination meets canvas. Perfect for all skill levels, unleash your creativity and transform inspiration into stunning works of art",
+        price: 250,
+        image:"images/art.png",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id:1003,
+        title:"Basketball Classes",
+        description: "Score skills and confidence in our basketball classes. Whether you're a beginner or honing your game, join us on the court for a slam-dunk combination of fun and fundamentals.",
+        price: 400,
+        image:"images/basketball.png",
+        availableInventory: 10,
+        rating: 2
+    
+    },
+    {
+        id:1004,
+        title:"Sculpture Classes",
+        description: "Shape your imagination with our sculpture classes. From beginners to experienced artists, sculpt, mold, and bring your ideas to life in a creative and supportive environment.",
+        price: 50.00,
+        image:"images/sculpture.png",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id:1005,
+        title:"Design Classes",
+        description: "Elevate your creativity with our design classes. Perfect for beginners or those looking to refine their skills, explore the world of design, and turn your ideas into visually stunning realities under expert guidance.",
+        price: 20.00,
+        image:"images/design.png",
+        availableInventory: 10,
+        rating: 5
+    
+    },
+    {
+        id:1006,
+        title:"Coding Classes",
+        description: "Unlock coding mastery in our classes—perfect for beginners or those leveling up. Dive into a supportive environment where innovation meets logic, shaping your coding journey.",
+        price: 50.00,
+        image:"images/coding.png",
+        availableInventory: 10,
+        rating: 5
+    },
+    {
+        id:1007,
+        title:"Music Classes",
+        description: "f dsf Discover the joy of music in our classes—perfect for all levels. Dive into a harmonious journey where every session brings your passion for music to life. dsf ds fsd fds gd",
+        price: 50.00,
+        image:"images/music.png",
+        availableInventory: 10,
+        rating: 1
+    },
+    {
+        id:1008,
+        title:"Photography Classes",
+        description: "Frame your story in our photography classes. Perfect for all levels, explore the art of visual storytelling and capture moments that matter",
+        price: 50.00,
+        image:"images/photo.png",
+        availableInventory: 10,
+        rating: 4
+    },
+    {
+        id:1009,
+        title:"Drama Class",
+        description: "Unleash your inner performer in our drama classes. From beginners to seasoned actors, join us for a stage-ready journey where creativity meets expression, and every scene is an opportunity to shine.",
+        price: 50.00,
+        image:"images/drama.png",
+        availableInventory: 10,
+        rating: 3
+    },
+    {
+        id:1010,
+        title:"Poetry Class",
+        description: "Craft your voice in our poetry classes. Perfect for all levels, join us for a poetic exploration where words come alive, and every verse is an expression of your unique perspective.",
+        price: 50.00,
+        image:"images/poetry.png",
+        availableInventory: 10,
+        rating: 4
+    }
+]
